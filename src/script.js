cad492c25e426092e4c16e0b9f5297eeed15a793
bharper99 <bethanyh.1999@gmail.com>
2021-11-15 02:14:23 +0000

    added get forecast function

--- src/script.js
+++ src/script.js
@@ -36,36 +36,59 @@ let dayAndTime = document.querySelector("#date");
 dayAndTime.innerHTML = formatDate();
 
 //DAILY FORECAST
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
 function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
- <div class="weather-forecast-date">${forecastDay.dt}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+ <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
 <img
 src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
 alt=""
 width="42"/>
 <div class="weather-forecast-temperature">
 <span class="weather-forecast-temperature-max"> ${Math.round(
-        forecastDay.temp.max
-      )}° 
+          forecastDay.temp.max
+        )}° 
         </span>
 <span class="weather-forecast-temperature-min"> ${Math.round(
-        forecastDay.temp.min
-      )}° </span>
+          forecastDay.temp.min
+        )}° </span>
  </div>
  </div>
 `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7059cb165caa3316bff682d263a01b1e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //CHANGES UNIT OF CURRENT TEMP
